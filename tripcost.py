0285d6c7a6376361a2d49a29cb4a1268ce3f10f3
Santhosh Kumar Jangam <111342834+santhoshkumarjangam@users.noreply.github.com>
2025-01-07 20:09:52 +0530

    Create tripcost.py

---
+++ tripcost.py
@@ -0,0 +1,7 @@
+def calculate_cost_of_trip(kilometers,milage,petrolprice):
+    print('total cost of the trip is going to be :',(kilometers/milage)*petrolprice)
+    
+calculate_cost_of_trip(50,10,100)
+#output:total cost of the trip is going to be : 500.0
+calculate_cost_of_trip(220,20,110)
+#output:total cost of the trip is going to be : 1210.0
